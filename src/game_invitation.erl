eada57ebc5d709c56044882db7c6fef5f9cb3079
Markus Flambard <markus@flambard.se>
2015-03-07 16:49:28 +0100

    Added functions for player color in game invitations.

--- src/game_invitation.erl
+++ src/game_invitation.erl
@@ -3,10 +3,14 @@
 %% API
 -export([ new/0
         , challenger/1
+        , challenger_color/1
+        , opponent_color/1
         ]).
 
 -record(invitation,
         { challenger
+        , challenger_color
+        , opponent_color
         }).
 
 
@@ -16,9 +20,15 @@
 
 new() ->
     #invitation{ challenger = self()
+               , challenger_color = white
+               , opponent_color = black
                }.
 
 challenger(#invitation{challenger = C}) -> C.
+
+challenger_color(#invitation{challenger_color = C}) -> C.
+
+opponent_color(#invitation{opponent_color = C}) -> C.
 
 
 %%%===================================================================
