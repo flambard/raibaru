95af90009ffff23a1e34c24f0f3d7719cf6b02b2
Markus Flambard <markus@flambard.se>
2015-03-07 16:50:25 +0100

    game:start_link should take the players as two arguments instead of in a list.

--- src/game.erl
+++ src/game.erl
@@ -2,7 +2,7 @@
 -behaviour(gen_server).
 
 %% API
--export([ start_link/1
+-export([ start_link/2
         ]).
 
 %% gen_server callbacks
@@ -22,8 +22,8 @@
 %%% API
 %%%===================================================================
 
-start_link(Players) ->
-    gen_server:start_link(?MODULE, Players, []).
+start_link(Player1, Player2) ->
+    gen_server:start_link(?MODULE, [Player1, Player2], []).
 
 
 %%%===================================================================
